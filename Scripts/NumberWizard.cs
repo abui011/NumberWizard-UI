391844c902cee7b7322fdf74f3cd5a739ec8a5fc
Alex Bui <alexbui@sorathas-MacBook-Pro.local>
2018-06-29 09:28:54 -0700

    2nd commit

---
+++ Scripts/NumberWizard.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour {
+    [SerializeField] int maxnum;
+    [SerializeField]int minnum;
+    [SerializeField]TextMeshProUGUI guessText;
+     int guess;
+	// Use this for initialization
+	//test github
+    void Start () {
+        guess = Random.Range(minnum, maxnum);
+        guessText.text = guess.ToString();
+	}
+
+    public void OnPressLower()
+    {
+        maxnum = guess;
+        guess = Random.Range(minnum, maxnum - 1);
+        guessText.text = guess.ToString();
+    }
+
+	public void OnPressHigher()
+    {
+        minnum = guess;
+        guess = Random.Range(minnum + 1, maxnum);
+        guessText.text = guess.ToString();
+    }
+
+
+	// Update is called once per frame
+}
---
+++ Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public void LoadNextScene(){
+        int cursceneindex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(cursceneindex + 1);
+    }
+
+    public void LoadStartScene(){
+        SceneManager.LoadScene(0);
+    }
+}
